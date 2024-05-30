63607a133462dcba5db0f923fcf955364cbaceb7
Shreejai Raj <shreejairaj@gmail.com>
2024-05-30 22:52:57 +1000

    schemes

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,5 +1,5 @@
 import SectionWrapper from "./SectionWrapper";
-import {WORKOUTS} from "../utils/swoldier.js"
+import {SCHEMES, WORKOUTS} from "../utils/swoldier.js"
 import { useState } from "react";
 
 function Header(props){
@@ -19,6 +19,9 @@ function Header(props){
 export default function Generator() {
 
   const [showModal, setShowModal] = useState(false);
+  const [poison, setPoison] = useState('individual');
+  const [muscles, setMuscles] = useState([]);
+  const [goals, setGoals] = useState('strength_power');
 
   function toggleModal() {
     setShowModal(!showModal)
@@ -45,6 +48,16 @@ export default function Generator() {
           <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
         </button>
         {showModal && (<div>modal</div>)}
+      </div>
+      <Header index={"03"} title={"Become Juggernaut"} description={"Select your ultimate objective."}/>
+      <div className="grid grid-cols-3 gap-4">
+        {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+          return (
+            <button className="bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3" key={schemeIndex}>
+              <p className="capitalize">{scheme.replaceAll('_', ' ')}</p>
+            </button>
+          )
+        })}
       </div>
       
     </SectionWrapper>
