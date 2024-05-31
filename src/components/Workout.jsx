939accd8cc737a12f04e85bc1bd341ae55fac6ae
Shreejai Raj <shreejairaj@gmail.com>
2024-05-31 15:50:31 +1000

    workouts

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,7 +1,19 @@
+import ExerciseCard from "./ExerciseCard"
+import SectionWrapper from "./SectionWrapper"
 
 export default function Workout(props) {
   const {workout} = props
   return (
-    <div>{workout}</div>
+    <SectionWrapper header={"Welcome to"} title={[
+      'The', 'DANGER', 'Zone'
+    ]}>
+      <div>
+        {workout.map((exercise, i)=>{
+          return(
+          <ExerciseCard i={i} exercise={exercise} key={i}/>
+          )
+        })}
+      </div>
+    </SectionWrapper>
   )
 }
